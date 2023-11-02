4195fbc91ef6bb94152e73e8e3e68faa4a7bfce3
Wien Peters <2106314peters@zuyd.nl>
2023-11-02 01:22:17 +0100

    Add project files.

---
+++ WeerTest/Class1.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace WeerTest
+{
+    internal class Class1
+    {
+        
+            // Setting up GeckoDriver
+        //new DriverManager().SetUpDriver(new FirefoxConfig());
+        //IWebDriver driver = new FirefoxDriver();
+
+        // Navigate to the website
+        //driver.Navigate().GoToUrl("http://example.com");
+
+        // Example: Find an input element and interact with it
+        //var inputElement = driver.FindElement(By.Name("inputName"));
+        //inputElement.SendKeys("Some Text");
+
+        // Add any additional actions, form submissions, or data scraping here
+
+        // Scrape data or perform further actions...
+
+        // Close the browser
+        //driver.Quit();
+    }
+}
---
+++ WeerTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the location for weather data:");
+        string location = Console.ReadLine();
+
+        string baseurl = "https://drimble.nl/weer/";
+        string finurl = $"{baseurl}{location}";
+        var web = new HtmlWeb();
+        var doc = web.Load(finurl);
+        var node = doc.DocumentNode.SelectSingleNode("//div[@class='dni top']");
+        string txt = node.WriteContentTo();
+        string replace = "&#176;C";
+        string beter = " Graden, de huidige temperatuur van";
+        if (txt.Contains(replace))
+        {
+            var weatherData = node.InnerText;
+            string niew = weatherData.Replace(replace, beter);
+            Console.WriteLine(niew);
+        }
+        else
+        {
+            Console.WriteLine("verkeerde stad als input");
+        }
+    }
+}
