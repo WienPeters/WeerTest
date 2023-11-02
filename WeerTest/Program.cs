81b5f0168dc0978260885c1581b5c6a285024810
Wien Peters <2106314peters@zuyd.nl>
2023-11-02 03:47:52 +0100

    weerlezer geupdatet

--- WeerTest/Program.cs
+++ WeerTest/Program.cs
@@ -5,6 +5,7 @@ using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
+using AngleSharp.Text;
 
 class Program
 {
@@ -19,21 +20,36 @@ class Program
         var doc = web.Load(finurl);
         var node = doc.DocumentNode.SelectSingleNode("//div[@class='dni top']");
         string txt = node.WriteContentTo();
+        //vervangt iets niet moois uitziend
         string replace = "&#176;C";
-        string beter = " Graden, is momenteel";
+        //vervangd het met duidelijkheid
+        string beter = " graden celcius.";
         if (txt.Contains(replace))
         {
             var weatherData = node.InnerText;
             string niew = weatherData.Replace(replace, beter);
             string input = niew;
+            //ruimte invoegen tussen aanelkaargeplakte tekst
             string stringToInsert = " ";
 
+            //patroon om hoofdlettertext wat aanelkaargeplakt is aan te pakken
             string pattern = "(?<=\\p{Lu}|)(?=\\p{Lu}|=d)";
+            //patroon om nummers te scheiden van text
             string pattern2 = @"(^|\n)[^\d]*\d";
             string result = Regex.Replace(input, pattern, stringToInsert);
+            //boven en onder het samenvoegen v.d. text
             string result2 = Regex.Replace(result, pattern2, m => m.Value.Insert(m.Value.Length - 1, stringToInsert), RegexOptions.Multiline);
-            //Console.WriteLine("Original String: " + input);
-            Console.WriteLine("Modified String: " + result2);
+            var s = result2[4]; var ss = result2[2]; var sss = result2[3];
+            string ses = (((ss.ToString() + (sss.ToString()))));
+            string ses2 = (((ses.Replace(',', ' '))));
+            int er = Int32.Parse(ses2);
+            bool zonpan = false;
+            
+            //Console.WriteLine(result2);
+            if ( er>19) { zonpan = true; Console.WriteLine("zonnenpanelen zijn met dit weer voordelig, "+ result2); }
+
+            else { Console.WriteLine("De huidige temperatuur :" + result2); }
+
             //
             //Console.WriteLine(niew);
         }
